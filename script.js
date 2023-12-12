85959cc1a741ef79df1b04f8a793eabe63d95ee6
Rohith-Daruri <139214300+Rohith-Daruri@users.noreply.github.com>
2023-12-12 08:03:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const extendHex = (shortHex) => {
-  // write your code here
+ let start = shortHex.length-3;
+	let iscapital = false;
+	let result ="#";
+	for(int i=start;i<shortHex.length;i++){
+		let val = shortHex.charAt(i);
+		if(val.charCodeAt(0)>=65&& val.charCodeAt(0)<=90){
+			iscapital = true;
+		}
+	result = result+shortHex.charAt(i)+shortHex.charAt(i);
+	}
+	if(iscapital==true){
+		return result.toUpperCase();
+	}
+	return result;
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+// const shortHex = prompt("Enter Short Hex.");
+// alert(extendHex(shortHex));
